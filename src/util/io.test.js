50edebb84ab0e5b6570e79550917092d7e37d9eb
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-10-10 18:04:51 -0500

    Adding __mocks__ folder to store custom mocks implementations

--- src/util/io.test.js
+++ src/util/io.test.js
@@ -21,7 +21,14 @@ it('should execute the writeFile method', () => {
 
     writeData(testData, testFileName);
 
-    // return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
-    // expect(fs.writeFile).toBeCalled();
     expect(fs.writeFile).toBeCalledWith(testFileName, testData);
+});
+
+it('should return a promise that resolves to no value if called correctly', () => {
+    const testData = 'Test';
+    const testFileName = 'test.txt';
+
+    writeData(testData, testFileName);
+
+    return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
 });
